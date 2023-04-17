936b1986d8dba9a1880e1dfefe2bb23aec1c6aa5
Qiu <qiu@QiudeMacBook-Pro.local>
2023-04-16 23:10:16 -0400

    finished the role elements animation

--- src/components/Header/hero/Hero.jsx
+++ src/components/Header/hero/Hero.jsx
@@ -4,7 +4,32 @@ import css from "./Hero.module.scss";
 const Hero = () => {
   return (
     <section className={`paddings ${css.wrapper}`}>
-      <div className={`innerWidth ${css.container}`}></div>
+      <div className={`innerWidth ${css.container}`}>
+        <img alt="picture of Xinran" src="../../../public/person.png" />
+        <div className={css.rightContent}>
+          <div class={css.role}>
+            <span class={css.part} id={css.user_experience}>
+              User Experience
+            </span>
+            <span class={css.part} id={css.designer}>
+              Designer
+            </span>
+            <span id={css.plus}>+</span>
+            <span class={css.part} id={css.front_end}>
+              Front-End
+            </span>
+            <span class={css.part} id={css.engineer}>
+              Engineer
+            </span>
+          </div>
+          <div className={css.introduction}>
+            My passion lies in bridging the gap between innovative UX design and
+            robust front-end development. I design user-centered solutions and
+            bring them to life, ensuring functionality, feasibility, and
+            efficiency.
+          </div>
+        </div>
+      </div>
     </section>
   );
 };
