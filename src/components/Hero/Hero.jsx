106591fcc193d2ed044e4a84f6b4a815653aa961
Qiu <qiu@QiudeMacBook-Pro.local>
2023-05-27 16:44:54 -0400

    re-style hero section

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -5,29 +5,31 @@ const Hero = () => {
   return (
     <section className={`paddings ${css.wrapper}`}>
       <div className={`innerWidth ${css.container}`}>
-        {/* <img alt="picture of Xinran" src="../../../public/person.png" /> */}
-        <div className={css.rightContent}>
-          <div class={css.role}>
-            <div className="secondaryText" id={css.uxd_text}>
-              User Experience Designer
-            </div>
-            <div className="secondaryText" id={css.fed_text}>
-              Front-End Developer
-            </div>
-            <div className="secondaryText" id={css.uxe_text}>
-              User Experience Engineer
-            </div>
-            <div className="secondaryText" id={css.plux_text}>
-              +
-            </div>
-          </div>
+        {/* <a href="https://www.freepik.com/free-vector/process-concept-illustration_11434267.htm#page=9&query=user%20experience%20engineer%20illustration&position=19&from_view=search&track=ais">Image by storyset</a> on Freepik */}
+        <img alt="picture of Xinran" src="../../../public/ux_process.png" />
 
-          <div className={css.introduction}>
-            My passion lies in bridging the gap between innovative UX design and
-            robust front-end development. I design user-centered solutions and
-            bring them to life, ensuring functionality, feasibility, and
-            efficiency.
+        <div className={`primaryText ${css.greeting}`}>Hi, I'm Xinran!</div>
+
+        <div class={css.role}>
+          <div className="secondaryText" id={css.uxd_text}>
+            User Experience Designer
           </div>
+          <div className="secondaryText" id={css.fed_text}>
+            Front-End Developer
+          </div>
+          <div className="secondaryText" id={css.uxe_text}>
+            User Experience Engineer
+          </div>
+          <div className="secondaryText" id={css.plux_text}>
+            +
+          </div>
+        </div>
+
+        <div className={css.introduction}>
+          My passion lies in bridging the gap between innovative UX design and
+          robust front-end development. I design user-centered solutions and
+          bring them to life, ensuring functionality, feasibility, and
+          efficiency.
         </div>
       </div>
     </section>
