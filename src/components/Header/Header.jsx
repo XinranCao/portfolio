8a7d594d659d6f0910e1dc98a856be28d8e5ab4d
Qiu <qiu@QiudeMacBook-Pro.local>
2023-05-27 16:54:45 -0400

    fix header background color

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -16,7 +16,7 @@ const Header = () => {
       whileInView="show"
       variants={headerVariants}
       viewport={{ once: false, amount: 0.25 }}
-      className={`paddings ${css.wrapper}`}
+      className={`paddings bg-primary  ${css.wrapper}`}
       style={{ boxShadow: headerShadow }}
     >
       <div className={`flexCenter innerWidth ${css.container}`}>
